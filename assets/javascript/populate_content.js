bf32ebcbcb40c0aa24e25e99be1b62134399bb37
nupurkapoor <nupurk15@gmail.com>
2015-06-16 00:19:53 -0400

    adding question indexing

--- assets/javascript/populate_content.js
+++ assets/javascript/populate_content.js
@@ -1,12 +1,12 @@
 
 function existingQuestions(iCounter){
-    $('.ada-form').each(function() {
-        $this = $(this);
-        var index = $.inArray($this.attr('id'), formElements);
-        if (index == -1) {
-            formElements.push($this.attr('id'));
-        };
-    })
-    qnIndex = formElements.length;
+  $('.ada-form').each(function() {
+      $this = $(this);
+      var index = $.inArray($this.attr('id'), formElements);
+      if (index == -1) {
+          formElements.push($this.attr('id'));
+      };
+  })
+  qnIndex = formElements.length;
 }
 
